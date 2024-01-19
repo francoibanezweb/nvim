3de00124b7453e066af9b34ff07404a0c8045e64
francoibanezweb <francoibanez.dev@gmail.com>
2024-01-19 11:37:13 -0300

    feat: update .gitignore

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,3 +1,13 @@
 -- You can add your own plugins here or in other files in this directory!
--- 
-return {}
+local plugins = {
+  -- Other plugins you might have
+	{
+	    "iamcco/markdown-preview.nvim",
+	    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	    ft = { "markdown" },
+	    build = function() vim.fn["mkdp#util#install"]() end,
+	}
+
+}
+
+return plugins
